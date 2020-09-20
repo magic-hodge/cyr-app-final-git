f4706eb5fc9346d15de3c7c71050358cf8bcf6c8
magic-hodge <nick.m.hodge@gmail.com>
2020-09-20 15:28:08 -0700

    handling mobile issues 1.

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 const Feedback = require('../models/feedback');
-const Student = require('../models/student');
+// const Student = require('../models/student');
 
 
 router.get('/', (req, res) => {
